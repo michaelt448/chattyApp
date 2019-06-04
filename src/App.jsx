d765cc1c614274bd2e499ac6639ce51e4de2f7e8
vagrant <mikegithub77@gmail.com>
2019-06-04 21:04:00 +0000

    modularized all html into diffrent classes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
-import React, {Component} from 'react';
-
+import React, { Component } from 'react';
+import MessageList from './MessageList.jsx';
+import ChatBar from './chatBar.jsx';
+// messageList 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser : {name: 'Bob'},
+      messages: []
+    }
+  }
+
+  componentDidMount(){
+
+  };
+
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+
+    <nav className="navbar">
+      <a href="/" className="navbar-brand">Chatty</a>
+    </nav>
+    <MessageList />
+    <ChatBar user={this.state.currentUser}/>
+      </div>
     );
   }
 }
